60376f68c82c7d83f3def0bdef096bac8e5d4232
KevStatic <kev.static11@gmail.com>
2026-02-09 12:54:58 +0530

    Initial project scaffolding and DB schema for ETMS
    Set up solution structure with empty interfaces, entities, and repositories for ETMS. Added initial SQL script to create Users, Employees, and TransferRequests tables with seed data. Updated project and solution files to reflect new structure. No business logic implemented yet.

---
+++ ETMS.Domain/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETMS.Domain.Entities
+{
+    internal class Users
+    {
+    }
+}
---
+++ ETMS.Infrastructure/Repositories/EmployeeRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETMS.Infrastructure.Repositories
+{
+    internal class EmployeeRepository
+    {
+    }
+}
